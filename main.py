3bea2d6ffee522b4c1eeef86ec6db9ed3f1966c2
Slawomir Napiorkowski <napiorkowski.slawomir@gmail.com>
2021-12-17 15:45:43 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+
+print("Zadanie 1")
+print("Lista zakupów")
+
+słownik = {
+    "Piekarnia": ["Chleb", "Pączek", "Bułki"], 
+    "Warzywniak": ["Marchew", "Seler", "Rukola"]
+    }
+for sklep in słownik:
+    print(f'Idę do {sklep}, kupuję tu następujące {słownik.get(sklep)}'),
+Ilość_zakupów_w_sklepie = (len(słownik.get("Piekarnia")))+(len(słownik.get("Warzywniak")))
+print(f"W sumie kupuję {Ilość_zakupów_w_sklepie} produktów.")
+
+
+    
+  
+
