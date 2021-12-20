f668b46e673be83d337795b11b30ec1d31411823
SlawekNap <96422532+SlawekNap@users.noreply.github.com>
2021-12-20 14:46:16 +0100

    zmiana w głównym pliku

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 
-print("Zadanie 1")
-print("Lista zakupów")
 
-słownik = {
-    "Piekarnia": ["Chleb", "Pączek", "Bułki"], 
-    "Warzywniak": ["Marchew", "Seler", "Rukola"]
-    }
-for sklep in słownik:
-    print(f'Idę do {sklep}, kupuję tu następujące {słownik.get(sklep)}'),
-Ilość_zakupów_w_sklepie = (len(słownik.get("Piekarnia")))+(len(słownik.get("Warzywniak")))
-print(f"W sumie kupuję {Ilość_zakupów_w_sklepie} produktów.")
-
-
+print("'Hiszpańska inkwizycja' to najlepszy skecz grupy Monty Pythona")
     
   
 
